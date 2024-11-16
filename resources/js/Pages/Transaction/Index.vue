c0fe554cca51c0b060e3f725921ba8ddfd695141
dingggjc <charlesaciertojc@gmail.com>
2024-11-16 10:13:53 +0800

    Implement validation for service inputs; update model and controller logic for products in transaction

--- resources/js/Pages/Transaction/Index.vue
+++ resources/js/Pages/Transaction/Index.vue
@@ -417,81 +417,84 @@ const props = defineProps({
                                 <!-- add ons -->
                                 <div class="hidden dark:bg-gray-800" id="contacts" role="tabpanel"
                                     aria-labelledby="contacts-tab">
-                                    <div
-                                        class="rounded-lg border border-gray-200 bg-white p-4  dark:border-gray-700 dark:bg-gray-800 md:p-6">
+                                    <div v-for="product in products" :key="product.product_inventory_id">
                                         <div
-                                            class="space-y-4 md:flex md:items-center md:justify-between md:gap-6 md:space-y-0">
+                                            class="rounded-lg border border-gray-200 bg-white p-4  dark:border-gray-700 dark:bg-gray-800 md:p-6">
+                                            <div
+                                                class="space-y-4 md:flex md:items-center md:justify-between md:gap-6 md:space-y-0">
 
-                                            <div class="flex items-center justify-between md:order-3 md:justify-end">
+                                                <div
+                                                    class="flex items-center justify-between md:order-3 md:justify-end">
 
-                                                <div class="text-end md:order-4 md:w-32">
-                                                    <p class="text-sm  text-red-900 dark:text-white">stock count - 20
-                                                    </p>
+                                                    <div class="text-end md:order-4 md:w-32">
+                                                        <p class="text-sm  text-red-900 dark:text-white">stock: {{
+                                                            parseInt(product.product_quantity) }}
+                                                        </p>
+                                                    </div>
+                                                </div>
+                                                <div
+                                                    class="w-full min-w-0 flex-1 pb-4 space-y-4 md:order-2 md:max-w-md">
+                                                    <h1
+                                                        class="text-sm font-medium text-gray-900 hover:underline dark:text-white">
+                                                        {{ product.product_name }}</h1>
+
+                                                    <a href="#"
+                                                        class="text-sm font-medium text-gray-900 hover:underline dark:text-white">
+                                                        {{ product.product_description }}
+                                                    </a>
+                                                </div>
+                                                <div
+                                                    class="flex items-center justify-between md:order-3 md:justify-end">
+                                                    <div class="flex items-center">
+                                                        <button type="button" id="decrement-button-2"
+                                                            data-input-counter-decrement="counter-input-2"
+                                                            class="inline-flex h-5 w-5 shrink-0 items-center justify-center rounded-md border border-gray-300 bg-gray-100 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-gray-100 dark:border-gray-600 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700">
+                                                            <svg class="h-2.5 w-2.5 text-gray-900 dark:text-white"
+                                                                aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                                                fill="none" viewBox="0 0 18 2">
+                                                                <path stroke="currentColor" stroke-linecap="round"
+                                                                    stroke-linejoin="round" stroke-width="2"
+                                                                    d="M1 1h16" />
+                                                            </svg>
+                                                        </button>
+                                                        <input type="text" id="counter-input-2" data-input-counter
+                                                            class="w-10 shrink-0 border-0 bg-transparent text-center text-sm font-medium text-gray-900 focus:outline-none focus:ring-0 dark:text-white"
+                                                            placeholder="" value="1" required />
+                                                        <button type="button" id="increment-button-2"
+                                                            data-input-counter-increment="counter-input-2"
+                                                            class="inline-flex h-5 w-5 shrink-0 items-center justify-center rounded-md border border-gray-300 bg-gray-100 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-gray-100 dark:border-gray-600 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700">
+                                                            <svg class="h-2.5 w-2.5 text-gray-900 dark:text-white"
+                                                                aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                                                fill="none" viewBox="0 0 18 18">
+                                                                <path stroke="currentColor" stroke-linecap="round"
+                                                                    stroke-linejoin="round" stroke-width="2"
+                                                                    d="M9 1v16M1 9h16" />
+                                                            </svg>
+                                                        </button>
+                                                    </div>
+                                                    <div class="text-end md:order-4 md:w-32">
+                                                        <p class="text-base font-bold text-gray-900 dark:text-white">
+                                                            ₱{{ product.product_price }}
+                                                        </p>
+                                                    </div>
                                                 </div>
                                             </div>
-                                            <div class="w-full min-w-0 flex-1 pb-4 space-y-4 md:order-2 md:max-w-md">
-                                                <h1
-                                                    class="text-sm font-medium text-gray-900 hover:underline dark:text-white">
-                                                    PACKAGE A Premium</h1>
-                                                <h1
-                                                    class="text-sm font-medium text-gray-900 pb-0 hover:underline dark:text-white">
-                                                    Small</h1>
 
-                                                <a href="#"
-                                                    class="text-sm font-medium text-gray-900 hover:underline dark:text-white">
-                                                    Body wash with foam shampoo
-                                                    Tire Black
-                                                    Vacuum
-                                                </a>
+                                            <div class="flex items-center gap-4">
+                                                <button type="button"
+                                                    class="px-3 py-2 text-xs font-medium text-center inline-flex items-center text-white bg-indigo-700 rounded-lg hover:bg-indigo-800 focus:ring-4 focus:outline-none focus:ring-indigo-300 dark:bg-indigo-600 dark:hover:bg-indigo-700 dark:focus:ring-indigo-800">
+                                                    <svg class="w-5 h-5 text-white me-2" aria-hidden="true"
+                                                        xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                                        fill="currentColor" viewBox="0 0 24 24">
+                                                        <path fill-rule="evenodd"
+                                                            d="M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12Zm13.707-1.293a1 1 0 0 0-1.414-1.414L11 12.586l-1.793-1.793a1 1 0 0 0-1.414 1.414l2.5 2.5a1 1 0 0 0 1.414 0l4-4Z"
+                                                            clip-rule="evenodd" />
+                                                    </svg>
+                                                    Choose Product
+                                                </button>
                                             </div>
-                                            <div class="flex items-center justify-between md:order-3 md:justify-end">
-                                                <div class="flex items-center">
-                                                    <button type="button" id="decrement-button-2"
-                                                        data-input-counter-decrement="counter-input-2"
-                                                        class="inline-flex h-5 w-5 shrink-0 items-center justify-center rounded-md border border-gray-300 bg-gray-100 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-gray-100 dark:border-gray-600 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700">
-                                                        <svg class="h-2.5 w-2.5 text-gray-900 dark:text-white"
-                                                            aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-                                                            fill="none" viewBox="0 0 18 2">
-                                                            <path stroke="currentColor" stroke-linecap="round"
-                                                                stroke-linejoin="round" stroke-width="2" d="M1 1h16" />
-                                                        </svg>
-                                                    </button>
-                                                    <input type="text" id="counter-input-2" data-input-counter
-                                                        class="w-10 shrink-0 border-0 bg-transparent text-center text-sm font-medium text-gray-900 focus:outline-none focus:ring-0 dark:text-white"
-                                                        placeholder="" value="1" required />
-                                                    <button type="button" id="increment-button-2"
-                                                        data-input-counter-increment="counter-input-2"
-                                                        class="inline-flex h-5 w-5 shrink-0 items-center justify-center rounded-md border border-gray-300 bg-gray-100 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-gray-100 dark:border-gray-600 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700">
-                                                        <svg class="h-2.5 w-2.5 text-gray-900 dark:text-white"
-                                                            aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-                                                            fill="none" viewBox="0 0 18 18">
-                                                            <path stroke="currentColor" stroke-linecap="round"
-                                                                stroke-linejoin="round" stroke-width="2"
-                                                                d="M9 1v16M1 9h16" />
-                                                        </svg>
-                                                    </button>
-                                                </div>
-                                                <div class="text-end md:order-4 md:w-32">
-                                                    <p class="text-base font-bold text-gray-900 dark:text-white">$598
-                                                    </p>
-                                                </div>
-                                            </div>
+
                                         </div>
-
-                                        <div class="flex items-center gap-4">
-                                            <button type="button"
-                                                class="px-3 py-2 text-xs font-medium text-center inline-flex items-center text-white bg-indigo-700 rounded-lg hover:bg-indigo-800 focus:ring-4 focus:outline-none focus:ring-indigo-300 dark:bg-indigo-600 dark:hover:bg-indigo-700 dark:focus:ring-indigo-800">
-                                                <svg class="w-5 h-5 text-white me-2" aria-hidden="true"
-                                                    xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                                    fill="currentColor" viewBox="0 0 24 24">
-                                                    <path fill-rule="evenodd"
-                                                        d="M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12Zm13.707-1.293a1 1 0 0 0-1.414-1.414L11 12.586l-1.793-1.793a1 1 0 0 0-1.414 1.414l2.5 2.5a1 1 0 0 0 1.414 0l4-4Z"
-                                                        clip-rule="evenodd" />
-                                                </svg>
-                                                Choose Product
-                                            </button>
-                                        </div>
-
                                     </div>
                                 </div>
                             </div>
