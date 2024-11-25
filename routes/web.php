8e6ed757b1238712379b1c45119a4c79c75b1ee8
dingggjc <charlesaciertojc@gmail.com>
2024-11-25 20:12:26 +0800

    Redirect landing page to login route

--- routes/web.php
+++ routes/web.php
@@ -24,13 +24,9 @@ use Inertia\Inertia;
 
 // Landing page
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
+    return redirect()->route('login');
 });
+
 
 // Dashboard
 Route::get('/dashboard', function () {
